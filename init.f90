067ecfd80229fc4e723392a1d6544b0b93fe38b6
gervasiozaldivar <gzaldivar@qi.fcen.uba.ar>
2023-09-18 19:14:55 -0300

    correct integration of long-range interactions

--- init.f90
+++ init.f90
@@ -4,7 +4,7 @@ use mfkfun
 n(1) = 1
 n(2) = n(3) ! number of Cl- equal to N+
 
-Xu = -lseg/5. ! vdw potential integral
+Xu = -2.961921959*lseg**3. ! vdw potential integral
 print*,"lseg is set to ",lseg
 print*,"Xu is set to ",Xu
 
